9525d733d99bbaeb503c633ffdd6e7a12e3ca707
Jesús Santiago <jesussantiagoespinosa@gmail.com>
2024-03-24 18:27:20 +0100

    Traducción de rutas y optimicación

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,4 @@ use App\Http\Controllers\SubastaController;
 
 Route::get('/', HomeController::class);
 
-Route::get('subastas', [SubastaController::class, 'index'])->name('subastas.index');
-
-Route::get('subastas/create', [SubastaController::class, 'create'])->name('subastas.create');
-
-Route::post('subastas', [SubastaController::class, 'store'])->name('subastas.store');
-
-Route::get('subastas/{subasta}', [SubastaController::class, 'show'])->name('subastas.show');
-
-Route::get('subastas/{subasta}/edit', [SubastaController::class, 'edit'])->name('subastas.edit');
-
-Route::put('subastas/{subasta}', [SubastaController::class, 'update'])->name('subastas.update');
-
-Route::delete('subastas/{subasta}', [SubastaController::class, 'destroy'])->name('subastas.destroy');
+Route::resource('subastas', SubastaController::class);
