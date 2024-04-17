0996b010a8e68bc98b4d8889b2f59fa51f3053ad
Jesús Santiago <jesussantiagoespinosa@gmail.com>
2024-04-17 20:53:18 +0200

    estilos de home y login hechos con bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,11 +12,13 @@
 
 </head>
 <body>
-    <div class="container-fluid">
+    <div class="container-fluid" style="margin-bottom: 40px">
         <div class="row">
-            <div class="bg-image" style="background-image: url('https://i.gyazo.com/a87b7ca685d14403197eb7382f5e0ec2.jpg'); height: 80vh;">   
+            <div class="bg-image" style="background-image: url('https://i.gyazo.com/a87b7ca685d14403197eb7382f5e0ec2.jpg'); background-attachment: fixed; background-repeat: no-repeat; background-size: cover">   
                 <div style="padding-top: 10px;">
-                    <img src="https://i.gyazo.com/4535fd5fe9889dbdaf5e02384f888481.png" class="img-fluid img-thumbnail" style="width: 10%; height: auto;" alt="Logo">
+                    <a href="{{route('home')}}">
+                        <img src="https://i.gyazo.com/4535fd5fe9889dbdaf5e02384f888481.png" class="img img-thumbnail" style="width: 15%; height: auto; min-width:200px" alt="Logo">
+                    </a>
                 </div>
                 <div class="text-white d-flex justify-content-center align-items-center" style="height: calc(80vh - 50px);">
                     <div class="text-center" style="font-family: 'Russo One'">
@@ -27,12 +29,17 @@
             </div>
         </div>
     </div>
-    <div>
-        <div class="container">
-            <a class="btn btn-primary btn-lg" href="{{route('iniciar-sesion')}}" role="button">Iniciar sesión</a>
-            <a class="btn btn-primary btn-lg" href="{{route('registro')}}" role="button">Regístrate</a>
-            {{-- <a href="{{route('iniciar-sesion')}}">Iniciar sesión</a>
-            <a href="{{route('registro')}}">Regístrate</a>              --}}
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-9" style="font-family: 'Russo One';">
+                <div style="margin-top: 5px" class="text-end">
+                    <h1 class="display-5" style="margin-right: 110px; text-shadow: 0px 8px 12px rgba(0, 0, 0, 0.2);">COMIENZA A COMPRAR, VENDER Y GANAR ORO</h1>
+                </div>
+            </div>
+            <div class="col-3 align-items-start">
+                <a class="btn btn-primary btn-lg" href="{{route('iniciar-sesion')}}" role="button" style="margin-top: 8px; margin-right: 20px; box-shadow: 0px 8px 12px rgba(0, 0, 0, 0.2);">Iniciar sesión</a>
+                <a class="btn btn-primary btn-lg" href="{{route('registro')}}" role="button" style="margin-top: 8px; box-shadow: 0px 8px 12px rgba(0, 0, 0, 0.2);">Regístrate</a>
+            </div>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,47 +1,62 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
+    <title>Iniciar sesión en Exchange</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Jersey+25&family=Russo+One&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Alata&family=Comfortaa:wght@300..700&family=Libre+Franklin:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
 </head>
-    <body>
-        <form method="POST" action="{{route('login')}}">
-          <h1>Iniciar sesión</h1>
-        
-        @csrf
-        
-        <label>
-            Email:
-            <br>
-            <input type="email" name="email">
-        </label>
-
-        <br>
-        
-        <label>
-            Contraseña:
-            <br>
-            <input type="password" name="password">
-        </label>
-
-        <br>
-                
-        <label>
-            Mantener sesión iniciada
-            <input type="checkbox" name="recordar">
-        </label>
-
-        <br>
-
-      ¿No tienes cuenta? <a href="{{route('registro')}}">Regístrate</a>
-
-        <br>
-
-      <button type="submit">
-        Entrar
-      </button>
-        </form>
-    </body>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="bg-image" style="background-image: url('https://i.gyazo.com/a87b7ca685d14403197eb7382f5e0ec2.jpg'); background-repeat: no-repeat; background-size: cover; height: 100vh">   
+                <div style="padding-top: 10px;">
+                    <a href="{{route('home')}}">
+                        <img src="https://i.gyazo.com/4535fd5fe9889dbdaf5e02384f888481.png" class="img img-thumbnail" style="width: 15%; height: auto; min-width:200px" alt="Logo">
+                    </a>
+                </div>
+                <div class="d-flex justify-content-center align-items-center" style="height: 90vh;">
+                    
+                        <div style="background-color: rgba(255, 255, 255, 0.5); border-radius: 20px;">
+                            <form method="POST" action="{{route('login')}}" class="p-4">
+                                <h1 class="display-4" style="font-family: 'Libre Franklin';font-weight: bold;">Iniciar sesión</h1>
+                                @csrf
+                                <div class="mb-3">
+                                    <label class="form-label w-100">
+                                        <h4>Email:</h4>
+                                        <input placeholder="Ingrese su correo electrónico" class="form-control w-100" type="email" name="email">
+                                    </label>
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label w-100">
+                                        <h4>Contraseña:</h4>
+                                        <input class="form-control" type="password" name="password">
+                                    </label>
+                                </div>
+                                <div class="mb-3 form-check">
+                                    <input type="checkbox" class="form-check-input" name="recordar">
+                                    <label class="form-check-label"><h5>Mantener sesión iniciada</h5></label>
+                                </div>
+                                <div class="mb-3">
+                                    <h6>¿No tienes cuenta?<a href="{{route('registro')}}"> Regístrate</h6></a>
+                                </div>
+                                <div>
+                                    <button type="submit" class="btn btn-primary">Iniciar sesión</button>
+                                </div>
+                            </form> 
+                        </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
 </html>
